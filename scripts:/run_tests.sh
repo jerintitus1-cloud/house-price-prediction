f398e82203ffd234bc77ff1d15c4c8290a90383b
jerintitus1-cloud <jerintitus1@gmail.com>
2026-07-09 16:22:54 +0530

    Delete scripts: directory

--- scripts:/run_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "🧪 Running Automated Test Suite via PyTest..."
-cd "$(dirname "$0")/.."
-
-# Execute pytest through the python module interface
-python3 -m pytest -v
-
-echo "🧹 Cleaning up bytecode artifacts..."
-find . -type d -name "__pycache__" -exec rm -rf {} +
